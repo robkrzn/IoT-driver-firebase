5f8b3e6b49fc4770dd856da09d0cb3c93ee1c096
robkrzn <robert1krizan2@gmail.com>
2021-02-20 22:36:14 +0100

    Add project files.

---
+++ IoT driver firebase/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace IoT_driver_firebase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button startGameButton;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.deviceComboBox = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.hryComboBox = new System.Windows.Forms.ComboBox();
+            startGameButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // startGameButton
+            // 
+            startGameButton.Location = new System.Drawing.Point(276, 17);
+            startGameButton.Name = "startGameButton";
+            startGameButton.Size = new System.Drawing.Size(126, 23);
+            startGameButton.TabIndex = 1;
+            startGameButton.Text = "Štart hry";
+            startGameButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.deviceComboBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(408, 74);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Vyber zariadenie";
+            // 
+            // deviceComboBox
+            // 
+            this.deviceComboBox.FormattingEnabled = true;
+            this.deviceComboBox.Location = new System.Drawing.Point(6, 29);
+            this.deviceComboBox.Name = "deviceComboBox";
+            this.deviceComboBox.Size = new System.Drawing.Size(165, 21);
+            this.deviceComboBox.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(startGameButton);
+            this.groupBox2.Controls.Add(this.hryComboBox);
+            this.groupBox2.Location = new System.Drawing.Point(12, 101);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(408, 85);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Nastavenie hry";
+            // 
+            // hryComboBox
+            // 
+            this.hryComboBox.FormattingEnabled = true;
+            this.hryComboBox.Location = new System.Drawing.Point(6, 19);
+            this.hryComboBox.Name = "hryComboBox";
+            this.hryComboBox.Size = new System.Drawing.Size(210, 21);
+            this.hryComboBox.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 217);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "IoT Driver";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox deviceComboBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox hryComboBox;
+    }
+}
+
---
+++ IoT driver firebase/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
+
+namespace IoT_driver_firebase
+{
+    public partial class Form1 : Form
+    {
+        private IFirebaseClient client;
+        private IFirebaseConfig config;
+        public Form1()
+        {
+            InitializeComponent();
+            this.config = new FirebaseConfig { AuthSecret = "AIzaSyCCGL7IOw-G4lA3idjYV73QHqkViHNtAHY", BasePath = "https://unikova-hra-default-rtdb.firebaseio.com/" };
+            this.client = new FireSharp.FirebaseClient(config);
+            if (this.client != null) {
+                MessageBox.Show("Sever pripojeny");
+            }
+        }
+        
+    }
+}
